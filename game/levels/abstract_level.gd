dc7b0600b96ec2a1bb8152a58f0e139b5c0f616b
Ruslan Ignatov <ruslanlena30@gmail.com>
2026-06-27 18:27:07 +0300

    Added player and abstract level

---
+++ game/levels/abstract_level.gd
@@ -0,0 +1,20 @@
+class_name AbstractLevel
+extends Node2D
+
+
+@onready var player : Player = $Player
+@onready var pause_menu : PauseMenu = $%PauseMenu
+
+
+func _ready() -> void:
+	pause_menu.hide()
+	
+	#TODO Add gameplay music
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		SoundManager.play_ui_stream(SoundManager.ui_stream_accept)
+		get_tree().paused = true
+		pause_menu.show()
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
